ae0884712dfd605826cf42160232a5c1ae8324d3
periapt <nicholas@periapt.co.uk>
2010-03-14 19:34:07 +0000

    Changed to Build.PL

--- lib/Math/StochasticProcess/Event.pm
+++ lib/Math/StochasticProcess/Event.pm
@@ -10,11 +10,11 @@ Math::StochasticProcess::Event - Base class for events.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
